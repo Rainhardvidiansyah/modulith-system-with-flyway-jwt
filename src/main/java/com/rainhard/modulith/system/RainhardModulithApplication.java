f2fa40c167be1ef5039c40fc25d73367ab4b2b82
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-18 13:22:40 +0700

    initial commit

---
+++ src/main/java/com/rainhard/modulith/system/RainhardModulithApplication.java
@@ -0,0 +1,13 @@
+package com.rainhard.modulith.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainhardModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainhardModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rainhard/modulith/system/RainhardModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainhard.modulith.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainhardModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
