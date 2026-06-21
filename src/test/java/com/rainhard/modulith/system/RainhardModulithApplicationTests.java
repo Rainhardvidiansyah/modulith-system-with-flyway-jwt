cb36fa0a8be1de3ac1fc08cbd769a962b3d78970
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-21 11:11:44 +0700

    test modular

--- src/test/java/com/rainhard/modulith/system/RainhardModulithApplicationTests.java
+++ src/test/java/com/rainhard/modulith/system/RainhardModulithApplicationTests.java
@@ -2,12 +2,19 @@ package com.rainhard.modulith.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootTest
 class RainhardModulithApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldVerifyModuleStructure(){
+		ApplicationModules modules = ApplicationModules.of(RainhardModulithApplication.class);
+		modules.verify();
 	}
 
 }
