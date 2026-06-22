2ecfbda44f7eea8bc1344cb1826a8c32b62945e3
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-22 11:28:00 +0700

    fixing parameter in product repository

--- src/main/java/com/rainhard/modulith/system/product/internal/ProductRepository.java
+++ src/main/java/com/rainhard/modulith/system/product/internal/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.rainhard.modulith.system.product.internal;
 
+import com.rainhard.modulith.system.product.dto.ProductRequest;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ProductRepository {
-    void saveProduct(String email, String password);
-    String getProductById(String id);
+    void saveProduct(ProductRequest productRequest);
+    Optional<ProductsEntity> getProductById(UUID id);
     String getProductByProductName(String productName);
-    void deleteProductById(String id);
+    void deleteProductById(UUID id);
     void getProductBySku(String sku);
 }
