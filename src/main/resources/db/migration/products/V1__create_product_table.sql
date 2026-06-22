d67fb1dfbc72e3ef705cea3d07670b5aa738d482
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-22 09:29:05 +0700

    created migration file: product table

---
+++ src/main/resources/db/migration/products/V1__create_product_table.sql
@@ -0,0 +1,12 @@
+-- V1__create_product_table.sql
+CREATE TABLE product (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price NUMERIC(12,2) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE INDEX idx_product_ud ON product (id);
+CREATE INDEX idx_product_name ON product (name);
