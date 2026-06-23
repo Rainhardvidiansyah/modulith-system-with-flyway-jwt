1186cf467633272616c04d3e6af733db1f5fc0d2
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-23 12:36:19 +0700

    create file jpa productrepository which extends jpa repository

---
+++ src/main/java/com/rainhard/modulith/system/product/internal/JpaProductRepository.java
@@ -0,0 +1,10 @@
+package com.rainhard.modulith.system.product.internal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+interface JpaProductRepository extends JpaRepository<Product, UUID> {
+}
