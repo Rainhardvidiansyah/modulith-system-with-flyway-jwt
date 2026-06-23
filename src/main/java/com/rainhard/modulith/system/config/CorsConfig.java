a7dbf56026546529cbc04f5aa0d9bf3ad8284589
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-23 21:18:44 +0700

    create cors config

---
+++ src/main/java/com/rainhard/modulith/system/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.rainhard.modulith.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**") // Pola URL API Anda
+                        .allowedOrigins("http://localhost:4200") // Domain Angular
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
