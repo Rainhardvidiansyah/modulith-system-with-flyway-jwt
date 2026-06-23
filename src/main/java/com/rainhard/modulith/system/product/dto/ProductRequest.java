2284662520b47b7605a65f2039c13a97788f5357
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-23 12:57:53 +0700

    create dto module that is containing product request and product response

---
+++ src/main/java/com/rainhard/modulith/system/product/dto/ProductRequest.java
@@ -0,0 +1,8 @@
+package com.rainhard.modulith.system.product.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRequest (
+        String name,
+        String description,
+        BigDecimal price) {}
