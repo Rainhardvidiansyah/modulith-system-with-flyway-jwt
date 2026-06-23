816cc62498b555c5c8ab608a2f4c385094956683
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-23 12:33:32 +0700

    create new migration file using timestamp ✌️✌️

---
+++ src/main/resources/db/migration/product/V20260623111208__seed_table_products.sql.sql
@@ -0,0 +1,25 @@
+-- V101__seed_products.sql
+-- Menambahkan data awal produk
+
+INSERT INTO products (name, description, price)
+VALUES
+(
+    'Laptop ThinkPad X1',
+    'Laptop bisnis dengan performa tangguh dan desain tipis.',
+    18500000.00
+),
+(
+    'Mechanical Keyboard K2',
+    'Keyboard mekanikal wireless dengan lampu RGB.',
+    950000.00
+),
+(
+    'Monitor 4K 27 inch',
+    'Monitor dengan resolusi tinggi untuk kebutuhan desain grafis.',
+    5200000.00
+),
+(
+    'Logitech Mouse MX3',
+    'Mouse ergonomis dengan presisi tinggi.',
+    1300000.00
+);
