1ac0c06d9149fc9761221f7f6f52f6687668ea96
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-02 21:39:30 +0700

    make api response as multiple class to get metadata when a request coming. ✅

--- src/main/java/com/rainhard/modulith/system/ApiResponse.java
+++ src/main/java/com/rainhard/modulith/system/ApiResponse.java
@@ -3,31 +3,71 @@ package com.rainhard.modulith.system;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+
 @AllArgsConstructor
 @Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
-
-    private boolean status;
-
-    private String message;
-
+    private Metadata metadata;
     private T data;
+    private ErrorDetail error;
 
-    private LocalDateTime timestamp;
 
-    public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(true, message, data, LocalDateTime.now());
-    }
-
-    public static <T> ApiResponse<T> error(String message, T data) {
-        return new ApiResponse<>(false, message, data, LocalDateTime.now());
-    }
 
     //TODO: implement metadata and pagination
+
+    @Getter
+    @Builder
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class Metadata{
+        private String requestId;
+        private Instant timestamp;
+        private int status;
+        private String path;
+        private String message;
+    }
+
+    @Getter
+    @Builder
+    public static class ErrorDetail{
+        private String code;
+        private String message;
+    }
+
+
+    public static <T> ApiResponse<T> success(int status, String path, String message, T data){
+        System.out.println("Di sukses: " + RequestFilter.getStringId());
+        return ApiResponse.<T>builder()
+                .metadata(Metadata.builder()
+                        .requestId(RequestFilter.getStringId())
+                        .timestamp(Instant.now())
+                        .status(status)
+                        .path(path)
+                        .message(message)
+                        .build())
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> error(int status, String path, String code, String message) {
+        System.out.println("Di error: " + RequestFilter.getStringId());
+        return ApiResponse.<T>builder()
+                .metadata(Metadata.builder()
+                        .requestId(RequestFilter.getStringId())
+                        .timestamp(Instant.now())
+                        .status(status)
+                        .path(path)
+                        .build())
+                .error(ErrorDetail.builder()
+                .code(code)
+                .message(message).build())
+                .build();
+    }
 
 }
