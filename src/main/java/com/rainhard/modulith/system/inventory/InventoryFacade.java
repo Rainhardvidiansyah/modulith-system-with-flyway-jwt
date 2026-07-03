21ad112448af8134945a891fa14625989285f346
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-03 11:08:23 +0700

    refactor InventoryFacade to returning a correct error--if an error occurred ✅✅

--- src/main/java/com/rainhard/modulith/system/inventory/InventoryFacade.java
+++ src/main/java/com/rainhard/modulith/system/inventory/InventoryFacade.java
@@ -1,12 +1,13 @@
 package com.rainhard.modulith.system.inventory;
 
+import com.rainhard.modulith.system.ResourceNotFoundException;
 import com.rainhard.modulith.system.inventory.dto.*;
 import com.rainhard.modulith.system.inventory.internal.InventoryService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -30,23 +31,31 @@ public class InventoryFacade {
     //READ OPERATIONS ===
 
     //Get Inventory By Inventory id
-    public Optional<InventoryResponse> getOneInventory(UUID inventoryId){
+    public InventoryResponse getOneInventory(UUID inventoryId){
         return inventoryService.getInventoryById(inventoryId)
-                .map(InventoryResponse::from);
+                .map(InventoryResponse::from)
+                .orElseThrow(() -> new ResourceNotFoundException("Inventory id not found: " + inventoryId));
     }
 
     //Get All Inventory
     public List<InventoryResponse> getAllInventories(){
-       return inventoryService.getAllInventories()
-               .stream()
-               .map(InventoryResponse::from)
-               .toList();
+        try{
+            return inventoryService.getAllInventories()
+                    .stream()
+                    .map(InventoryResponse::from)
+                    .toList();
+        } catch (NoSuchElementException e) {
+            throw new ResourceNotFoundException("Inventories not listed");
+        }
     }
 
+
     //Find Inventory by ProductId
-    public Optional<InventoryResponse> getInventoryByProductId(UUID productId){
+    public InventoryResponse getInventoryByProductId(UUID productId){
         return inventoryService.getInventoryByProductId(productId)
-                .map(InventoryResponse::from);
+                .map(InventoryResponse::from)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Inventory with productId: " + productId + " not found") );
     }
 
 
@@ -63,8 +72,12 @@ public class InventoryFacade {
 
     //Restock
     public InventoryResponse restockInventory (UUID inventoryId, int quantity, BigDecimal costPrice){
-        var inventory = inventoryService.restock(inventoryId, quantity, costPrice);
-        return InventoryResponse.from(inventory);
+        try{
+            var inventory = inventoryService.restock(inventoryId, quantity, costPrice);
+            return InventoryResponse.from(inventory);
+        }catch (NoSuchElementException e){
+            throw new ResourceNotFoundException("Inventory id not found: " + inventoryId);
+        }
     }
 
 }
