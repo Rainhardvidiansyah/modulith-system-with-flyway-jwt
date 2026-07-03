51dd3b643951ad4e8cdc5d1c53d7d7d9acdfa2a3
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-03 11:09:41 +0700

    removed response entity from controller ❌❌; make it return simple thing as the other have been taken care by other class ✅✅

--- src/main/java/com/rainhard/modulith/system/inventory/controller/InventoryController.java
+++ src/main/java/com/rainhard/modulith/system/inventory/controller/InventoryController.java
@@ -1,16 +1,13 @@
 package com.rainhard.modulith.system.inventory.controller;
 
 
-import com.rainhard.modulith.system.ApiResponse;
+
 import com.rainhard.modulith.system.inventory.InventoryFacade;
 import com.rainhard.modulith.system.inventory.dto.InventoryBatchResponse;
 import com.rainhard.modulith.system.inventory.dto.InventoryResponse;
 import com.rainhard.modulith.system.inventory.dto.InventoryRestockRequest;
-import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -41,60 +38,45 @@ public class InventoryController {
 
     //FIND inventory by InventoryId
     @GetMapping("/get/{inventoryId}")
-    public ResponseEntity<ApiResponse<InventoryResponse>> findInventoryById(@PathVariable("inventoryId") UUID inventoryId){
-        LOGGER.info("Method hit");
+    public InventoryResponse findInventoryById(@PathVariable("inventoryId") UUID inventoryId){
 
-        LOGGER.info("Inventory id is: {}", inventoryId);
 
-        return inventoryFacade.getOneInventory(inventoryId)
-                        .map(response -> (ResponseEntity.ok(ApiResponse.success("Inventory successfully retrieved", response))))
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.error("Inventory Not found", null)));
+        LOGGER.info("Inventory id in findInventoryById method is: {}", inventoryId);
+
+        return inventoryFacade.getOneInventory(inventoryId);
     }
 
-    //FIND All Inventory
+
+//    //FIND All Inventory
     @GetMapping("/")
-    public ResponseEntity<ApiResponse<List<InventoryResponse>>> findAllInventory(){
-        var inventories = inventoryFacade.getAllInventories();
-
-        if(inventories.isEmpty()){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(ApiResponse.error("Inventories not Found", null));
-        }
-
-        return ResponseEntity.ok(ApiResponse.success("All Inventories successfully retrieved", inventories));
+    public List<InventoryResponse> findAllInventory(){
+        return inventoryFacade.getAllInventories();
     }
 
-    //FIND Inventory by ProductID
+
+//    //FIND Inventory by ProductID
     @GetMapping("/product/{productId}")
-    public ResponseEntity<@NonNull ApiResponse<InventoryResponse>> getInventoryByProductId(@PathVariable("productId") UUID productId){
-
-        return inventoryFacade.getInventoryByProductId(productId)
-                .map(response -> ResponseEntity.ok(ApiResponse.success("Inventory retrieved successfully", response)))
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.error("Inventory not found", null)));
-    }
-
-    //FIND batch by Inventory id
-    @GetMapping("/{inventoryId}")
-    public ResponseEntity<ApiResponse<List<InventoryBatchResponse>>> findBatchByInventoryId(@PathVariable("inventoryId") UUID inventoryId){
-        var inventoryBatches = inventoryFacade.findBatchesByInventoryId(inventoryId);
-
-        if(inventoryBatches.isEmpty()){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(ApiResponse.error("Inventory batches not Found", null));
-        }
-
-        return ResponseEntity.ok(ApiResponse.success("All Inventory batches successfully retrieved", inventoryBatches));
+    public InventoryResponse getInventoryByProductId(@PathVariable("productId") UUID productId){
+        return inventoryFacade.getInventoryByProductId(productId);
     }
 
 
-    //RESTOCK Data
+//    //FIND batch by Inventory id
+    @GetMapping("/batch/{inventoryId}")
+    public List<InventoryBatchResponse> findBatchByInventoryId(@PathVariable("inventoryId") UUID inventoryId){
+        LOGGER.info("Method find batch by inventory id get hit...");
+        return inventoryFacade.findBatchesByInventoryId(inventoryId);
+    }
+
+
+//    //RESTOCK Data
     @PostMapping("/{inventoryId}/restock")
-    public ResponseEntity<ApiResponse<InventoryResponse>> restockInventory(@PathVariable("inventoryId") UUID inventoryId,
+    public InventoryResponse restockInventory(@PathVariable("inventoryId") UUID inventoryId,
                                                                            @RequestBody InventoryRestockRequest restockRequest){
         LOGGER.info("Restock method is hit");
         var restockData = inventoryFacade.restockInventory(inventoryId, restockRequest.getQuantity(), restockRequest.getCostPrice());
         LOGGER.info("Data for restock: {}", restockData);
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("Restock is success", restockData));
+        return restockData;
     }
 
 
