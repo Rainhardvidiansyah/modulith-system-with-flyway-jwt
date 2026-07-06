e341f1386e784866dd6c64b64579f420e13ac979
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-06 11:24:18 +0700

    Extends JpaRepository and make one method to find expired date in Order

--- src/main/java/com/rainhard/modulith/system/order/internal/JpaOrderRepository.java
+++ src/main/java/com/rainhard/modulith/system/order/internal/JpaOrderRepository.java
@@ -1,4 +1,17 @@
 package com.rainhard.modulith.system.order.internal;
 
-public interface JpaOrderRepository {
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface JpaOrderRepository extends JpaRepository<Order, UUID> {
+
+    @Query("SELECT o FROM Order o WHERE o.status = 'PENDING' AND o.expiredAt < :now  ")
+    List<Order> findExpireOrderDate(@Param("now")Instant now);
 }
