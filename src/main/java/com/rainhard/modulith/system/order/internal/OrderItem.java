9c4fc54536c3ba0f60ef1829f98fcb26bc9d6819
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-06 11:25:28 +0700

    add getter annotation in OrderItem.java ✅

--- src/main/java/com/rainhard/modulith/system/order/internal/OrderItem.java
+++ src/main/java/com/rainhard/modulith/system/order/internal/OrderItem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.math.BigDecimal;
@@ -11,6 +12,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "order_items")
+@Getter
 public class OrderItem {
 
     @Id
