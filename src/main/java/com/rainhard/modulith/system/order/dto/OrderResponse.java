4c3fa72b52470bcae674ac9504041462eae2c2c9
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-06 11:31:52 +0700

    added several new field in OrderResponse class ✅✅✅

--- src/main/java/com/rainhard/modulith/system/order/dto/OrderResponse.java
+++ src/main/java/com/rainhard/modulith/system/order/dto/OrderResponse.java
@@ -1,4 +1,31 @@
 package com.rainhard.modulith.system.order.dto;
 
-public class OrderResponse {
+import com.rainhard.modulith.system.order.internal.OrderResult;
+import com.rainhard.modulith.system.order.internal.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public record OrderResponse(UUID orderId,
+                            UUID userId,
+                            OrderStatus status,
+                            BigDecimal totalAmount,
+                            List<OrderItemResponse> items,
+                            Instant createdAt) {
+
+    public static OrderResponse from(OrderResult orderResult){
+        return new OrderResponse(
+                orderResult.order().getId(),
+                orderResult.order().getUserId(),
+                orderResult.order().getStatus(),
+                orderResult.order().getTotalAmount(),
+                orderResult.items().stream().map(
+                        OrderItemResponse::from)
+                        .toList(),
+                orderResult.order().getCreatedAt()
+        );
+    }
+
 }
