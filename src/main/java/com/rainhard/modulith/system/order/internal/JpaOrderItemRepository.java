286797e0403969d4efff6e4861fa1a294e352f4a
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-07-06 11:23:12 +0700

    add new method find order item by order id in JpaOrderItemRepository

--- src/main/java/com/rainhard/modulith/system/order/internal/JpaOrderItemRepository.java
+++ src/main/java/com/rainhard/modulith/system/order/internal/JpaOrderItemRepository.java
@@ -3,8 +3,11 @@ package com.rainhard.modulith.system.order.internal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface JpaOrderItemRepository extends JpaRepository<OrderItem, UUID> {
+
+    List<OrderItem> findOrderItemByOrderId(UUID orderId);
 }
